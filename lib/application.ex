2bb5b7c37ebdfd242951ad3d767934bfd260c005
Beltrán Aceves <beltran.aceves@udc.es>
2024-05-12 22:45:49 +0200

    Remove faulty GenServer registry

--- lib/application.ex
+++ lib/application.ex
@@ -7,7 +7,6 @@ defmodule GenEditor.Application do
   def start(_type, _args) do
     Kino.SmartCell.register(GenEditor.ElementEditor)
     # TODO: rename the project to be in line with Kino widelines. Ex: kino_gen, kino_gen_editor
-    # TODO: register the global generable element Store here. Use a GenServer
     children = []
     opts = [strategy: :one_for_one, name: KinoDB.Supervisor]
     Supervisor.start_link(children, opts)
