6aed903ae667ffc7d5709f64361a3aa45f651fc4
Beltrán Aceves <beltran.aceves@udc.es>
2024-05-21 22:56:55 +0200

    Scaffolding server side validation module

---
+++ lib/gen_editor/validation.ex
@@ -0,0 +1,26 @@
+defmodule GenEditor.Validation do
+
+  def check_module_name_validity!(name) do
+    unless inspect(name) =~ Regex.recompile!(~r/^[A-Z]\w*(\.[A-Z]\w*)*$/) do
+      Mix.raise(
+        "Module name must be a valid Elixir alias (for example: Foo.Bar), got: #{inspect(name)}"
+      )
+    end
+  end
+  def check_app_name!(name, from_app_flag) do
+    unless name =~ Regex.recompile!(~r/^[a-z][\w_]*$/) do
+      extra =
+        if !from_app_flag do
+          ". The application name is inferred from the path, if you'd like to " <>
+            "explicitly name the application then use the `--app APP` option."
+        else
+          ""
+        end
+
+      Mix.raise(
+        "Application name must start with a letter and have only lowercase " <>
+          "letters, numbers and underscore, got: #{inspect(name)}" <> extra
+      )
+    end
+  end
+end
