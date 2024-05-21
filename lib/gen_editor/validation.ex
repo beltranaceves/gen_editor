61963ce882456e3405c200507aeda1a12d3e124a
Beltrán Aceves <beltran.aceves@udc.es>
2024-05-21 23:41:20 +0200

    Name app validation

--- lib/gen_editor/validation.ex
+++ lib/gen_editor/validation.ex
@@ -4,20 +4,8 @@ defmodule GenEditor.Validation do
     name |> String.match?(~r/^[A-Z]\w*(\.[A-Z]\w*)*$/)
   end
 
-  def check_app_name!(name, from_app_flag) do
-    unless name =~ Regex.recompile!(~r/^[a-z][\w_]*$/) do
-      extra =
-        if !from_app_flag do
-          ". The application name is inferred from the path, if you'd like to " <>
-            "explicitly name the application then use the `--app APP` option."
-        else
-          ""
-        end
-
-      Mix.raise(
-        "Application name must start with a letter and have only lowercase " <>
-          "letters, numbers and underscore, got: #{inspect(name)}" <> extra
-      )
-    end
+  def check_app_name!(name) do
+    name |> String.match?(~r/^[a-z][\w_]*$/)
   end
+
 end
