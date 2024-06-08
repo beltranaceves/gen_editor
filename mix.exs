56c0179bd94b0836deea4fe17d04e0987e660606
Beltrán Aceves <beltran.aceves@udc.es>
2024-06-08 13:53:58 +0200

    bumped version to test cloud

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule GenEditor.MixProject do
     [
       app: :gen_editor,
       description: "Kino smart cells for Phoenix's phx.gen commands",
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
